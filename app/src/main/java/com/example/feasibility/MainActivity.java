d2769b1770520bd6acf498dcac69e1ede30f09cd
Cody Chinn <cdchinn14@gmail.com>
2020-01-25 14:40:23 -0500

    Updating for feasiblity

--- app/src/main/java/com/example/feasibility/MainActivity.java
+++ app/src/main/java/com/example/feasibility/MainActivity.java
@@ -2,9 +2,20 @@ package com.example.feasibility;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
     Button greetings, farewell;
@@ -29,5 +40,29 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView)findViewById(R.id.screenText)).setText("Goodbye Forever");
             }
         });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String url = "https://dummy.restapiexample.com/api/v1/employees";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        ((TextView)findViewById(R.id.restView)).setText("Response: " + response.toString());
+                        Log.e("Rest Response ", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.e("Rest Error", error.toString());
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+
     }
 }

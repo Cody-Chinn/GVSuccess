795b4e9f9aab995ad8de79229ac2f24e9464b9d3
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2020-02-11 14:46:14 -0500

    Chemistry page setup

--- app/src/main/java/com/example/gvsuccess/MainActivity.java
+++ app/src/main/java/com/example/gvsuccess/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.gvsuccess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gvsuccess.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button chem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chem = findViewById(R.id.chemB);
+        chem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openChem();
+            }
+        });
+    }
+
+    public void openChem(){
+        Intent intent = new Intent(this, chemPage.class);
+        startActivity(intent);
     }
 }

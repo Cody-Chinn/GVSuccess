3a7b2f93cd17b1706e6d815a4e4296dbc9004c37
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2020-02-21 12:34:43 -0500

    Additional spinner features

--- app/src/main/java/com/example/gvsuccess/chemPage.java
+++ app/src/main/java/com/example/gvsuccess/chemPage.java
@@ -2,13 +2,31 @@ package com.example.gvsuccess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class chemPage extends AppCompatActivity {
+    private Button scheduleB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chem_page);
+
+        //Button setup
+        scheduleB = findViewById(R.id.schedule);
+        scheduleB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSchedule();
+            }
+        });
+    }
+
+    public void openSchedule() {
+        Intent intent = new Intent(this, schedulingPage.class);
+        startActivity(intent);
     }
 }

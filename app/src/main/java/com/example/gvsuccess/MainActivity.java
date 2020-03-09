837fa9cfe3460f12f5bdcae2eabbb630fbfd8724
Cody Chinn <cdchinn14@gmail.com>
2020-03-09 16:24:46 -0400

    google sign in functioning

--- app/src/main/java/com/example/gvsuccess/MainActivity.java
+++ app/src/main/java/com/example/gvsuccess/MainActivity.java
@@ -8,7 +8,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -19,10 +22,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    final private String TAG ="Main Activity";
     private RecyclerView recyclerView;
     private Adapter adapter;
     private ArrayList<SuccessCenter> items;
     private Context context;
+
+    private String mGoogleUsername;
 
     // Access a Cloud Firestore instance from your Activity
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -35,6 +41,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         items = new ArrayList<>();
         context = this;
+
+        try {
+            // Get the users account that is logged in and set the title
+            Bundle mExtras = getIntent().getExtras();
+            mGoogleUsername = mExtras.getString("account");
+
+            setTitle("Welcome, " + mGoogleUsername);
+
+        } catch(Exception e) {
+            Log.e(TAG, e.toString());
+        }
 
         db.collection("success centers")
         .get()

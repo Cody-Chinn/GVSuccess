674e7dd3fbc9a7925cd64cef60455ecfeba5f393
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2020-04-05 14:11:50 -0400

    Attempted reconfiguration of database

--- app/src/main/java/com/example/gvsuccess/LoginActivity.java
+++ app/src/main/java/com/example/gvsuccess/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.gvsuccess;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,8 +17,14 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.internal.GoogleApiAvailabilityCache;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -30,14 +37,16 @@ public class LoginActivity extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient;
     final int RC_SIGN_IN = 1;
     private DataAccess da = new DataAccess();
+    private FirebaseAuth fbAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        fbAuth = FirebaseAuth.getInstance();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                //.requestIdToken(getString(R.string.server_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -60,7 +69,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
-
+        FirebaseUser current = fbAuth.getCurrentUser();
+        updateUI(current);
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
 
@@ -80,8 +90,33 @@ public class LoginActivity extends AppCompatActivity {
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                GoogleSignInAccount a = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(a);
+            }catch(ApiException apie) {
+                Log.w(TAG, "Google SignIn Failure", apie);
+            }
         }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount ac) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + ac.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(ac.getIdToken(), null);
+        fbAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCreditail: success");
+                            FirebaseUser user = fbAuth.getCurrentUser();
+                            updateUI(user);
+                        }
+                        else {
+                            Log.w(TAG, "signInWithCredential: fail", task.getException());
+                        }
+                    }
+                });
     }
 
     private void signIn() {
@@ -94,16 +129,15 @@ public class LoginActivity extends AppCompatActivity {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            updateUI(account);
+            //updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
         }
     }
 
-    private void updateUI(GoogleSignInAccount account){
+    private void updateUI(FirebaseUser account){
         Task<QuerySnapshot> tutors = da.getTutors();
         final ArrayList<String> ts = new ArrayList<>();
         Intent openScreen;
